a298f6bf92f6ceb435e5b676b1f836caad391c2e
milind595 <63394659+milind595@users.noreply.github.com>
2020-04-16 18:53:13 +0530

    Add files via upload

---
+++ random_number.py
@@ -0,0 +1,16 @@
+import random
+Number = random.randint(1,101)
+
+enter_number = input('Enter Number:')
+
+if enter_number == Number:
+    print('correct')
+else:
+      print('incorrect')
+
+
+
+
+print('Number:' , Number)        
+    
+
